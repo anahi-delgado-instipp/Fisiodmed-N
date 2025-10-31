6f5ed14e1d0fdbad9ef8eda833d02e04c2b6b3c1
AnahiDelgado <adelgado316_est@instipp.edu.ec>
2025-10-31 03:19:28 -0500

    Configuración de base de datos PostgreSQL y despliegue en Render

--- fisiodmed_project/settings.py
+++ fisiodmed_project/settings.py
@@ -91,13 +91,23 @@ WSGI_APPLICATION = 'fisiodmed_project.wsgi.application'
     #}
 #}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+import dj_database_url
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://usuario:contraseña@host:puerto/nombre_db',
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 
 # Password validation
